c5ce6983091b18d9d074465d1a878e52ebddfbc1
GuoJinMing@mbp <tdouguo@163.com>
2021-09-13 16:10:58 +0800

    更新最新版

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# 更新记录
+
+## 20201101版
+
+此版本是依据《仙鹤大叔张文鹤》公众号 20201101 ‘灯火阑珊’整理版
+
+原文地址: https://mp.weixin.qq.com/s/OH8hQpRSVAkcRU3tQMMQ9Q
+
+## 20201101版
+
+使用: https://vuepress.vuejs.org/zh/guide/
+
+此版本是依据《仙鹤大叔张文鹤》公众号 20201101 ‘灯火阑珊’整理版
+
+原文地址: https://mp.weixin.qq.com/s/OH8hQpRSVAkcRU3tQMMQ9Q
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: '鹤叔医疗科普合集',
+    // plugins: ['fulltext-search'],
+    themeConfig: {
+        sidebar: 'auto',
+        smoothScroll: true,
+        nav: [
+            { text: '最新版', link: '/' },
+            { text: '20200322版', link: '/20200322/' },
+        ]
+    }
+}
