13abd7b37890cbc8c902f09c329ee2f4b449ac27
GuoJinMing@mbp <tdouguo@163.com>
2021-09-13 16:17:23 +0800

    百度统计

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,5 +8,28 @@ module.exports = {
             { text: '最新版', link: '/' },
             { text: '20200322版', link: '/20200322/' },
         ]
-    }
+    },
+
+    head: [
+        // 添加百度统计
+        [
+            "script",
+            {},
+            `
+            var _hmt = _hmt || [];
+            (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?0431ff8e640f1ee6d6943f1672d0d6c2";
+            var s = document.getElementsByTagName("script")[0]; 
+            s.parentNode.insertBefore(hm, s);
+            })();
+
+            host = window.location.host; 
+            if (host!="hs.tdouguo.com") 
+            { 
+                window.location.href='http://hs.tdouguo.com'; 
+            }
+            `
+        ]
+    ]
 }
